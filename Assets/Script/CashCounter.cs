b2d11de1ef8554291a5c932f26104a506e62c3c8
Tafsir Rahman adro <rtafsir342@gmail.com>
2026-02-22 22:04:37 +0600

    Income Multiplier Added

--- Assets/Script/CashCounter.cs
+++ Assets/Script/CashCounter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CashCounter : PlayerTapDetection
@@ -5,9 +6,28 @@ public class CashCounter : PlayerTapDetection
     [SerializeField] private Animator CounterAnim;
     [SerializeField] private Animator PerticaleAnim;
 
+    private Coroutine boostCoroutine;
+
     protected override void OnTap()
     {
         PerticaleAnim.SetTrigger("Action");
         CounterAnim.SetTrigger("Tapped");
+
+        // restart the boost every tap
+        if (boostCoroutine != null) StopCoroutine(boostCoroutine);
+        boostCoroutine = StartCoroutine(IncomeBoost());
     }
+
+IEnumerator IncomeBoost()
+{
+    int current = GameManager.instance.GetIncomePerSecond();
+    int bonus   = current;
+
+    GameManager.instance.RegisterIncome(bonus);
+
+    yield return new WaitForSeconds(0.2f);
+
+    GameManager.instance.UnregisterIncome(bonus);
+    boostCoroutine = null;
+}
 }
